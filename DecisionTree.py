aa8cdb45b96127bd6c2eb57f82a333d7a8ff2234
18satwik <41579386+18satwik@users.noreply.github.com>
2021-03-27 19:23:02 +0530

    Create DecisionTree.py

---
+++ DecisionTree.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 10:26:27 2021
+
+@author: satwik
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+import random
+from keras.datasets import fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+print("Shape of x_train: {}".format(x_train.shape))
+print("Shape of y_train: {}".format(y_train.shape))
+print()
+print("Shape of x_test: {}".format(x_test.shape))
+print("Shape of y_test: {}".format(y_test.shape))
+
+labelNames = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+sample = 1902 
+each = x_train[sample]
+
+plt.figure(figsize=(3,3))
+plt.imshow(each)
+plt.colorbar()
+plt.show()
+print("Image (#{}): Which is label number '{}', or label '{}''".format(sample,y_train[sample], labelNames[y_train[sample]]))
+
+ROW = 7
+COLUMN = 7
+plt.figure(figsize=(10, 10)) 
+for i in range(ROW * COLUMN):
+    temp = random.randint(0, len(x_train)+1)
+    image = x_train[temp] 
+    plt.subplot(ROW, COLUMN, i+1)       
+    plt.imshow(image, cmap='gray')  
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel(labelNames[y_train[temp]])
+    plt.tight_layout()   
+    
+plt.show()
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255.0
+x_test /= 255.0
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2])
+print(x_train.shape)
+print(x_test.shape)
+
+tree = DecisionTreeClassifier(max_depth=100, criterion='entropy')
+tree.fit(x_train, y_train)
+tree_pred=tree.predict(x_test)
+conf_mat = metrics.confusion_matrix(y_test,tree_pred)
+
+print(metrics.classification_report(y_test, tree_pred))
+accuracy = metrics.accuracy_score(y_test, tree_pred)
+average_accuracy = np.mean(y_test == tree_pred) * 100
+print("The average_accuracy is {0:.1f}%".format(average_accuracy))
+print('\nConfusion Matrix: \n',conf_mat)
+
+plt.matshow(conf_mat)
+plt.title('Confusion Matrix for Validation Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
